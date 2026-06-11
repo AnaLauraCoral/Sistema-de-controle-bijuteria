9b66dd4298a25818cd276098c0262c5ac61c6743
Ana Laura Coral Peruchi <ana.lauracp@outlook.com>
2026-06-11 14:09:03 -0300

    Update Sistema_Controle_Biju.java
    array

--- Sistema_bijuteria_ana/Sistema_Controle_Biju.java
+++ Sistema_bijuteria_ana/Sistema_Controle_Biju.java
@@ -1,7 +1,7 @@
-public class Sistema_Controle_Biju {
+
 
     //----classe das pessoas(super)----
-public class Pessoa {
+class Pessoa {
     protected int codigo;
     protected String nome;
     
@@ -18,7 +18,7 @@ public class Pessoa {
 
 
     //----classe dos clientes(pertence a pessoas)----
-public class Cliente extends Pessoa {
+class Cliente extends Pessoa {
     private String telefone;
 
     public Cliente(int codigo, String nome, String telefone) {
@@ -35,7 +35,7 @@ public class Cliente extends Pessoa {
 
 
     //----classe dos funcionario(pertence a pessoas)----
-public class Funcionario extends Pessoa {
+class Funcionario extends Pessoa {
     private String cargo;
 
     public Funcionario(int codigo, String nome, String cargo) {
@@ -53,7 +53,7 @@ public class Funcionario extends Pessoa {
 
 
     //----classe das peças(super)----
-public class Peca {
+class Peca {
     protected int codigo;
     protected String nome;
     protected double preco;
@@ -72,7 +72,7 @@ public class Peca {
 }
 
     //----classe dos colares(pertence a peças)----
-public class Colar extends Peca {
+class Colar extends Peca {
     private String material;
 
     public Colar(int codigo, String nome, double preco, String material) {
@@ -88,7 +88,7 @@ public class Colar extends Peca {
 }
 
     //----classe dos brincos(pertence a peças)----
-public class Brinco extends Peca {
+class Brinco extends Peca {
     private String tamanho;
 
     public Brinco(int codigo, String nome, double preco, String tamanho) {
@@ -102,4 +102,59 @@ public class Brinco extends Peca {
         System.out.println("Tamanho: " + tamanho);
     }
 }
+
+//array
+
+
+public class Sistema_Controle_Biju {
+
+    static Cliente[] clientes = new Cliente[10];
+    static Funcionario[] funcionarios = new Funcionario[10];
+    static Peca[] pecas = new Peca[10];
+
+    static int qtdClientes = 0;
+    static int qtdFuncionarios = 0;
+    static int qtdPecas = 0;
+
+    public static void main(String[] args) {
+
+        // Clientes
+        clientes[qtdClientes++] =
+                new Cliente(1, "Ana", "99999-9999");
+
+        clientes[qtdClientes++] =
+                new Cliente(2, "Carlos", "98888-8888");
+
+        // Funcionários
+        funcionarios[qtdFuncionarios++] =
+                new Funcionario(1, "Maria", "Vendedora");
+
+        funcionarios[qtdFuncionarios++] =
+                new Funcionario(2, "Pedro", "Gerente");
+
+        // Peças
+        pecas[qtdPecas++] =
+                new Colar(1, "Colar de Pérolas", 49.90, "Pérola");
+
+        pecas[qtdPecas++] =
+                new Brinco(2, "Brinco Argola", 29.90, "Médio");
+
+        System.out.println("=== CLIENTES ===");
+        for (int i = 0; i < qtdClientes; i++) {
+            clientes[i].exibirDados();
+            System.out.println("----------------");
+        }
+
+        System.out.println("\n=== FUNCIONÁRIOS ===");
+        for (int i = 0; i < qtdFuncionarios; i++) {
+            funcionarios[i].exibirDados();
+            System.out.println("----------------");
+        }
+
+        System.out.println("\n=== PEÇAS ===");
+        for (int i = 0; i < qtdPecas; i++) {
+            pecas[i].exibirDados();
+            System.out.println("----------------");
+        }
+    }
 }
