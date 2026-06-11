cd9a9bbd1bde9128c4c2335da36586b80280c9b0
Ana Laura Coral Peruchi <ana.lauracp@outlook.com>
2026-06-11 13:57:58 -0300

    Update Sistema_Controle_Biju.java
    herança 2

--- Sistema_bijuteria_ana/Sistema_Controle_Biju.java
+++ Sistema_bijuteria_ana/Sistema_Controle_Biju.java
@@ -17,7 +17,7 @@ public class Pessoa {
 }
 
 
-    //----classe dos clientes(pertence as pessoas)----
+    //----classe dos clientes(pertence a pessoas)----
 public class Cliente extends Pessoa {
     private String telefone;
 
@@ -34,7 +34,7 @@ public class Cliente extends Pessoa {
 }
 
 
-    //----classe dos funcionario(pertence as pessoas)----
+    //----classe dos funcionario(pertence a pessoas)----
 public class Funcionario extends Pessoa {
     private String cargo;
 
@@ -51,4 +51,55 @@ public class Funcionario extends Pessoa {
 }
 
 
+
+    //----classe das peças(super)----
+public class Peca {
+    protected int codigo;
+    protected String nome;
+    protected double preco;
+
+    public Peca(int codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void exibirDados() {
+        System.out.println("Código: " + codigo);
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: R$ " + preco);
+    }
+}
+
+    //----classe dos colares(pertence a peças)----
+public class Colar extends Peca {
+    private String material;
+
+    public Colar(int codigo, String nome, double preco, String material) {
+        super(codigo, nome, preco);
+        this.material = material;
+    }
+
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("Material: " + material);
+    }
+}
+
+    //----classe dos brincos(pertence a peças)----
+public class Brinco extends Peca {
+    private String tamanho;
+
+    public Brinco(int codigo, String nome, double preco, String tamanho) {
+        super(codigo, nome, preco);
+        this.tamanho = tamanho;
+    }
+
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("Tamanho: " + tamanho);
+    }
+}
 }
